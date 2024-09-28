e4332b52f42a1d23575e98a881a260707523655b
RingoTangs <singrainyang@gmail.com>
2024-09-28 15:24:17 +0800

    ✨ feat(config): element-plus 添加国际化

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+</script>
+
 <template>
-  <RouterView></RouterView>
+  <el-config-provider :locale="zhCn">
+    <RouterView></RouterView>
+  </el-config-provider>
 </template>
 
 <style>
