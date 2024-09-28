a6f21df270fc34e255d499a97c787ad3b0a3787b
RingoTangs <singrainyang@gmail.com>
2024-09-28 11:08:10 +0800

    ✨ feat(config): vite inspect

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import legacy from '@vitejs/plugin-legacy'
-import AutoImport from 'unplugin-auto-import/vite'
-import UnoCSS from 'unocss/vite'
+import viteLegacy from '@vitejs/plugin-legacy'
+import autoImport from 'unplugin-auto-import/vite'
+import unoCSS from 'unocss/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import viteInspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    legacy(),
-    AutoImport({
+    viteLegacy(),
+    autoImport({
       imports: ['vue'],
       dts: './types/auto-imports.d.ts',
       dirs: ['./src/**'],
       eslintrc: { enabled: true },
     }),
-    UnoCSS(),
+    unoCSS(),
+    vueDevTools(),
+    viteInspect(),
   ],
   build: {
     cssTarget: 'chrome49',
