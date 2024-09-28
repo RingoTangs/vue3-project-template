cb2fbec4883223a3f97a34feb7c8014e36f4eab0
RingoTangs <singrainyang@gmail.com>
2024-09-28 10:39:31 +0800

    ✨ feat(config): 配置AutoImport

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import legacy from '@vitejs/plugin-legacy'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), legacy()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    legacy(),
+    AutoImport({
+      imports: ['vue'],
+      dts: './types/auto-imports.d.ts',
+      dirs: ['./src/**'],
+      eslintrc: { enabled: true },
+    }),
+  ],
   build: {
     cssTarget: 'chrome49',
   },
